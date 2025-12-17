920196bdec5b8328381282ca15953d00b4cf68b3
Naeem Mohsin <miannaeem.mct@gmail.com>
2025-12-17 16:54:01 +0500

    fourth commit

--- frontend_book/docusaurus.config.ts
+++ frontend_book/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://your-username.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/textbook/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'your-username', // Your GitHub org/user name.
